3baf70ed1cd76d1749c26865251e11d5787af9d5
chris-petrevski <chris.petrevski@ajgrantgroup.com.au>
2022-01-28 13:35:43 +1100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     $('#navbar-toggler').click(function(){
         $('.navbar-collapse').slideToggle(400);
     });
+
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+        if(pos >= 100) {
+            $('.navbar').addClass('cgn-navbar');
+        } else {
+            $('.navbar').removeClass('cgn-navbar')
+        }
+    });
 });
